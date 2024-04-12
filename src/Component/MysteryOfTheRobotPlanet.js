28eed47e1dabba07e81495b8072bc894badb3584
Harsh kumar Yaduvanshi <2000hyhy@gmail.com>
2024-04-12 12:02:08 +0530

    first1

--- src/Component/MysteryOfTheRobotPlanet.js
+++ src/Component/MysteryOfTheRobotPlanet.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import '../New.css';
+import './New.css';
+import '../Card.css';
 
 const MysteryOfTheRobotPlanet = ({ data }) => {
   const { Title, Image, Wordexplore } = data;
 
   return (
-    <div className="container">
-      <div className="mystery-container">
+    <div className="container1">
+      <div className="mystery-container1">
         <img src={`https://ik.imagekit.io/dev24/${Image[0]}`} alt={Title} className="card-image1" />
         <h1 className="card-title1">{Title}</h1>
       </div>
 
-      <div className="word-container">
+      <div className="word-container1">
         {Wordexplore.map((word) => (
           <div key={word._id} className="word-card1">
-            <img src={`https://ik.imagekit.io/dev24/${word.Storyimage[0]}`} alt={word.Storytitle} className="word-image" />
-            <h3 className="word-title">{word.Storytitle}</h3>
-            <p className="word-text">{word.Storyitext}</p>
+            <img src={`https://ik.imagekit.io/dev24/${word.Storyimage[0]}`} alt={word.Storytitle} className="word-image1" />
+            <h3 className="word-title1">{word.Storytitle}</h3>
+            <p className="word-text1">{word.Storyitext}</p>
           </div>
         ))}
       </div>
